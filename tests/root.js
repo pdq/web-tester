73ed4de7163b4a6edaa5165bf47d9152801993e0
Victor Perez <zartora@Victors-MacBook-Pro.local>
2021-03-11 19:36:41 -0700

    PDQ test

--- tests/root.js
+++ tests/root.js
@@ -1,27 +1,49 @@
 module.exports = {
-  // you can search for content with the quick search bar
+
+  // This runs before every test.
+  beforeEach: browser => {
+    browser.url('https://www.pdq.com')
+  },
+
+  // This runs after every test.
+  afterEach: browser => {
+    browser.end()
+  },
+
+  // Searching for content with the quick search bar.
   quick_search_works: function (browser) {
     browser
-      .url('https://www.pdq.com')
       .waitForElementVisible('body', 1000)
       .setValue('input[type=text]', 'PowerShell')
       .keys('')
-      .assert.urlContains('?q=Powershell')
-      .end()
+      .assert.urlContains('?q=PowerShell')
   },
 
-  // dropdown menus are navigable
+  // Dropdown menus are navigable.
   dropdown_menus_work: function (browser) {
     browser
-      .url('https://www.pdq.com')
       .waitForElementVisible('body', 1000)
-      .moveToElement('button[data-test-id=resources]', 100, 100, function () {
-        browser.assert.visible('[data-test-id]=blog-menu-button')
+      .moveToElement('button[data-test-id="resources"]','button.css-1lxi5g6','a[data-test-id="blog-menu-button"]', function () {
+        browser
+          .assert.visible('a[data-test-id="blog-menu-button"]')
       })
-      .end()
   },
 
-  // all footer links work
+  // Free trial link takes you to a registration page.
+  free_trial_links_work: function (browser) {
+    browser
+      .click('[data-test-id=start-trial-cta-link]')
+      .assert.urlContains('Register')
+  },
+  
+  // Checking for link on main page to schedule a demo.
+  demo_link_exists: function (browser) {
+    browser
+      .waitForElementVisible('body', 3000)
+      .assert.visible('a[href*="https://app.hubspot.com/meetings/kelly-hammer/45-minute-product-demo"]')
+  },
+
+  // All footer links work.
   footer_links_work: function (browser) {
     const footerlinks = [
       'PDQ Deploy',
@@ -47,27 +69,19 @@ module.exports = {
       'Privacy Policy',
       'Terms of Use'
     ]
-    footerlinks.forEach(potentialFooterLink => {
-      browser.assert.visible(potentialFooterLink)
-    })
-    browser.end()
-  },
-
-  // free trial link takes you to a registration page
-  free_trial_links_work: function (browser) {
     browser
-      .url('https://www.pdq.com')
-      .waitForElementVisible('body', 1000)
-      .click(['data-test-id=start-trial-cta-link'])
-      .end()
-  },
+      .waitForElementVisible('body', 3000)
+      for(var x = 0; x < 22; x++){
+        browser
+          .useXpath()
+          .click(`(//li/a[text()[contains(.,"${footerlinks[x]}")]])`)
+          .url('https://www.pdq.com')
+      }
 
-  // we have a link somewhere on the page to schedule a demo
-  demo_link_exists: function (browser) {
-    browser
-      .url('https://www.pdq.com')
-      .waitForElementVisible('body', 1000)
-      .assert.visible(['href=https://app.hubspot.com/meetings/kelly-hammer/45-minute-product-demo'])
-      .end()
-  }
+      //I couldn't get the forEach to work for me, I made a work around using the For function
+
+      // footerlinks.forEach(potentialFooterLink => {
+      //   browser.assert.visible(potentialFooterLink)
+      // })
+  },
 }
