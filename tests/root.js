2a92b2eb5b1f4abafeabe025093bad3b4e885030
Anusha Nalla <anushanalla@example.com>
2021-03-15 23:28:03 -0600

    Fixing failed test cases as part of assessment.

--- tests/root.js
+++ tests/root.js
@@ -1,32 +1,50 @@
 module.exports = {
+  open_browser: function(browser) {
+	  browser.url('https://www.pdq.com')
+	  .waitForElementVisible('body', 1000)
+	  .maximizeWindow()
+  },
   // you can search for content with the quick search bar
   quick_search_works: function (browser) {
     browser
-      .url('https://www.pdq.com')
-      .waitForElementVisible('body', 1000)
       .setValue('input[type=text]', 'PowerShell')
       .keys('')
-      .assert.urlContains('?q=Powershell')
-      .end()
+      .assert.urlContains('?q=PowerShell')
   },
 
   // dropdown menus are navigable
   dropdown_menus_work: function (browser) {
+	
     browser
-      .url('https://www.pdq.com')
-      .waitForElementVisible('body', 1000)
       .moveToElement('button[data-test-id=resources]', 100, 100, function () {
-        browser.assert.visible('[data-test-id]=blog-menu-button')
+    	browser.click('button[data-test-id=resources]')
+        browser.assert.visible('a[data-test-id=blog-menu-button]')
       })
-      .end()
-  },
+  	},
 
-  // all footer links work
+  // we have a link somewhere on the page to schedule a demo
+  demo_link_exists: function (browser) {
+	browser.back()
+	browser.url('https://www.pdq.com')
+	  .waitForElementVisible('body', 1000)
+    browser.expect.element('Schedule a demo', 'link text').to.be.visible;
+  },
+  
+//free trial link takes you to a registration page
+  free_trial_links_work: function (browser) {
+    browser
+      .click('a[data-test-id=start-trial-cta-link]')
+  },
+  
+//all footer links work
   footer_links_work: function (browser) {
+	  browser.back()
+	  browser
+	  .execute('window.scrollTo(0,document.body.scrollHeight);');
     const footerlinks = [
+      'Enterprise SL',
       'PDQ Deploy',
       'PDQ Inventory',
-      'Enterprise SL',
       'Pricing',
       'Downloads',
       'Licensing',
@@ -48,26 +66,9 @@ module.exports = {
       'Terms of Use'
     ]
     footerlinks.forEach(potentialFooterLink => {
-      browser.assert.visible(potentialFooterLink)
+    	browser.expect.element(potentialFooterLink, 'link text').to.be.visible;
+      //browser.assert.visible(potentialFooterLink)
     })
     browser.end()
-  },
-
-  // free trial link takes you to a registration page
-  free_trial_links_work: function (browser) {
-    browser
-      .url('https://www.pdq.com')
-      .waitForElementVisible('body', 1000)
-      .click(['data-test-id=start-trial-cta-link'])
-      .end()
-  },
-
-  // we have a link somewhere on the page to schedule a demo
-  demo_link_exists: function (browser) {
-    browser
-      .url('https://www.pdq.com')
-      .waitForElementVisible('body', 1000)
-      .assert.visible(['href=https://app.hubspot.com/meetings/kelly-hammer/45-minute-product-demo'])
-      .end()
   }
 }
